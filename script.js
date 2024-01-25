e900d3b1d13d46c2fc0081746ab38d032948d1b1
shrutiparadkar2004 <shrutiparadkar93@gmail.com>
2024-01-25 13:30:19 +0530

    javascript programming

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function run(){
     let htmlCode = document.getElementById
-    ("html-code")
+    ("html-code");
 
     let cssCode = document.getElementById
-    ("css-code")
+    ("css-code");
 
     let jsCode = document.getElementById
-    ("js-code")
+    ("js-code");
+    
+    let output = document.getElementById
+    ("output");
 
+    output.contentDocument.body.innerHTML =
+    htmlCode.value;
 }
